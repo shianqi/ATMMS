b15675e76abcd209ac51047abd718384da064ba1
shianqi <shianqi@imudges.com>
2016-05-19 19:33:51 +0800

    显示编号完成

--- src/com/ATMMS/imudges/service/ItemService.java
+++ src/com/ATMMS/imudges/service/ItemService.java
@@ -5,11 +5,16 @@ import java.util.Date;
 import java.util.List;
 
 import org.apache.struts2.ServletActionContext;
+import org.junit.Test;
 
 import com.ATMMS.imudges.DAO.Factory;
 import com.ATMMS.imudges.DAO.FactoryDAO;
 import com.ATMMS.imudges.DAO.Item;
 import com.ATMMS.imudges.DAO.ItemDAO;
+import com.ATMMS.imudges.DAO.Major;
+import com.ATMMS.imudges.DAO.MajorDAO;
+import com.ATMMS.imudges.DAO.Subsystem;
+import com.ATMMS.imudges.DAO.SubsystemDAO;
 
 public class ItemService {
 	public void getAllItem(){
@@ -20,8 +25,10 @@ public class ItemService {
 	
 	public boolean showItemById(int id){
 		try {
+			
 			ItemDAO itemDAO = new ItemDAO();
 			Item item = itemDAO.findById(id);
+			item.setNum(getNumber(id));
 			ServletActionContext.getRequest().setAttribute("item", item);
 			return true;
 		} catch (Exception e) {
@@ -43,5 +50,51 @@ public class ItemService {
 		item.setRemark(remark);
 		itemDAO.save(item);
 		return item.getId();
+	}
+	
+	public String getNumber(int id){
+		String str = "";
+		int num = 0;
+		String itemString = "";
+		String factoryString = "";
+		String subsystemString = "";
+		String majorString = "";
+		
+		MajorDAO majorDAO = new MajorDAO();
+		SubsystemDAO subsystemDAO = new SubsystemDAO();
+		FactoryDAO factoryDAO = new FactoryDAO();
+		ItemDAO itemDAO = new ItemDAO();
+		
+		Item item = itemDAO.findById(id);
+		List<Item> items = itemDAO.findByProject(item.getProject());
+		
+		for(int i=0;i<items.size();i++){
+			if(items.get(i).getId()==id){
+				itemString = items.get(i).getProject()+"-"+(i+1);
+				num = Integer.valueOf(items.get(i).getParent());
+				break;
+			}
+		}
+		
+		Factory factory = factoryDAO.findById(num);
+		factoryString = factory.getNum();
+		num = Integer.valueOf(factory.getParent());
+		
+		Subsystem subsystem = subsystemDAO.findById(num);
+		List<Subsystem> listSubsystems = subsystemDAO.findByParent(subsystem.getParent());
+		//System.out.println(listSubsystems.size());
+		for(int i=0;i<listSubsystems.size();i++){
+			if(listSubsystems.get(i).getId()==num){
+				subsystemString = (i+1)+"";
+				num = Integer.valueOf(listSubsystems.get(i).getParent()); 
+				break;
+			}
+		}
+		
+		String[] majorNum = {"¢ñ","¢ò","¢ó","¢ô","¢õ","¢ö","¢÷","¢ø","¢ù","¢ú"};
+		majorString = majorNum[num];
+		
+		str = majorString+"-"+subsystemString+"-"+factoryString+"-"+itemString;
+		return str;
 	}
 }
